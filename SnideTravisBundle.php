b8c34dbd820e6097bf58b38d66b3c0b1696e6e23
Pascal DENIS <pascal.denis@businessdecision.com>
2014-03-08 22:22:43 +0100

    base

---
+++ SnideTravisBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the SnideTravisBundle bundle.
+ *
+ * (c) Pascal DENIS <pascal.denis.75@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snide\Bundle\TravisBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SnideTravisBundle
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class SnideTravisBundle extends Bundle
+{
+}
